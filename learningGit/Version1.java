63c45c4239c45ac5bfc76745fa3568291d9e9177
Sameer-Maniyar <sameermaniyar156@gmail.com>
2023-05-20 20:53:50 +0530

    feature2

--- learningGit/Version1.java
+++ learningGit/Version1.java
@@ -6,6 +6,7 @@ public class Version1 {
 	public static void main(String[] args) {
 		
 		System.out.println("i am in master branch");
+		System.out.println("bug fix in branch name fearture 2");
 		
 	}
 
